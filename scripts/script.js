f115566d60c7c7bc57a5cc95bcffcf8e03303749
Valyria <76444716+itsValyria@users.noreply.github.com>
2023-09-13 23:20:09 +0200

    fixed basically everything on the to do list :)

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,16 @@
 function changetheme() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    const isDarkMode = body.classList.contains('dark-mode');
+
+    const toggle = document.querySelector('i');
+    toggle.classList.toggle('fa-regular');
+    toggle.classList.toggle('fa-solid');
+
+    const hero = document.querySelector('.header > img');
+    if (isDarkMode) {
+        hero.src = 'assets/darkmode.png';
+    } else {
+        hero.src = 'assets/lightmode.png';
+    }
+}
