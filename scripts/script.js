64100fe9d50a870808d7678418b2c82fabe7ea07
Valyria <76444716+itsValyria@users.noreply.github.com>
2023-09-18 22:57:14 +0200

    birthday function

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,30 @@
+// Dark Theme Function
+
 function changetheme() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-    const isDarkMode = body.classList.contains('dark-mode');
+    const body = document.body
+    body.classList.toggle('dark-mode')
+    const isDarkMode = body.classList.contains('dark-mode')
 
-    const toggle = document.querySelector('i');
-    toggle.classList.toggle('fa-regular');
-    toggle.classList.toggle('fa-solid');
+    //Toggled tussen de regular en filled icon, depending on dark mode activated of niet.
+    const toggle = document.querySelector('i') 
+    toggle.classList.toggle('fa-regular')
+    toggle.classList.toggle('fa-solid')
 
-    const hero = document.querySelector('.header > img');
-    if (isDarkMode) {
-        hero.src = 'assets/darkmode.png';
-    } else {
-        hero.src = 'assets/lightmode.png';
+    const hero = document.querySelector('.header > img') // Selecteert de IMG in de header
+    if (isDarkMode) { // Als de website op dark mode staat, dan
+        hero.src = 'assets/darkmode.png' // is de image darkmode.png
+    } else { // Anders,
+        hero.src = 'assets/lightmode.png' // is de image lightmode.png
     }
 }
+
+// Birthday Function
+
+let now = new Date() // Maakt een 'Date' object voor vandaag.
+document.querySelectorAll('[data-birthday]').forEach((element => { // Haalt alle HTML elementen op die een 'data-birthday' attribuut hebben.
+    let date = new Date(Date.parse(element.dataset.birthday)) // Maakt een 'Date' object voor de opgegeven birthday aan.
+
+    if (date.getDate() == now.getDate() && date.getMonth() == now.getMonth()){ // Dit checkt dat de dag van de maand en de maand gelijk zijn.
+        element.classList.remove('hidden') // Als de datum van vandaag matcht met een birthday, haalt hij de class 'hidden' weg.
+    }
+}))
